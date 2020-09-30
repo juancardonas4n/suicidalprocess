204a7ec95882086d7a8b9ebe182e8224b334ca5d
Juan Francisco Cardona McCormick <juancardona@seven4n.com>
2020-09-29 20:38:13 -0500

    Adding two different process

---
+++ src/normalprocess.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <time.h>
+#include <cstdio>
+#include <csignal>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string>
+#include <iostream>
+
+int
+main(int argc, 
+     char *argv[], 
+     char *env[]) {
+
+  for (;;) { 
+
+    int input;
+
+    std::cin >> input;
+    std::cout << (input + 1) << std::endl;
+  }
+  return 0; /* Muerte despues de una vida productiva */
+}
